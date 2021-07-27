5f75fae57a24e13709f6c7ba6b020d57410e95b8
Cintia <cintia.braz@unesp.br>
2021-07-27 18:40:35 -0300

    Algumas funções

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var nome="Cintia";
+//alert("Olá" + nome);
+//Isto é um comentário
+console.log(nome);
+var frase = "Eu gosto de batata";
+
+console.log(frase.replace("batata", "sorvete"));
+//alert(frase.replace("batata", "sorvete"));
+//alert(frase.toUpperCase());
+
+var lista = ["Francês","Português","Inglês"];
+//alert(lista[1]);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.join(" - "));
+
+var linguas = [{nome: "Francês", nivel:"Básico"}, {nome: "Inglês", nivel:"Avançado"}];
+
+console.log(linguas);
+alert(linguas.nivel);
