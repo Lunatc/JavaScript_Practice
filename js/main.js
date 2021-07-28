cd97da26bdc2dba311ecc73fe4a62085faa5e9cd
Cintia <cintia.braz@unesp.br>
2021-07-28 11:28:22 -0300

    Functions

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var nome="Cintia";
+/*var nome="Cintia";
 //alert("Olá" + nome);
 //Isto é um comentário
 console.log(nome);
@@ -18,3 +18,42 @@ var linguas = [{nome: "Francês", nivel:"Básico"}, {nome: "Inglês", nivel:"Ava
 
 console.log(linguas);
 alert(linguas.nivel);
+*/
+
+/*
+var idade = prompt("Qual a sua idade?");
+
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}
+*/
+/*
+var count = 0;
+
+while(count < 5){
+    console.log(count);
+    count = count + 1; //count++;
+};
+
+for(count=0; count<=5; count++){
+    alert(count);
+}
+
+var d = new Date();
+alert(d.getHours());
+alert(d.getMonth()+1); //começa em 0
+
+
+function soma(n1,n2){
+    return n1 + n2;
+}
+
+*/
+
+function botao(){
+    //alert("Obrigada por clicar");
+    document.getElementById("thanks"). innerHTML = "<b>Obrigado por clicar</b>";
+    console.log(document.getElementById("thanks"));
+}
